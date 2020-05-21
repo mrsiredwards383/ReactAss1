fe53822ba36b0e807da8b57bca8257e5fa28f690
[mrsiredwards] <ryanmedwards@gmail.com>
2020-05-21 19:24:58 -0400

    Add State

--- src/App.js
+++ src/App.js
@@ -1,16 +1,27 @@
-import React from 'react';
+import React, { Component } from 'react';
 import './App.css';
 import './UserInput.js';
 import './UserOutput.js';
 import UserInput from './UserInput.js';
+import UserOutput from './UserOutput.js';
 
-function App() {
-  return (
-    <div className="App">
-      Hello
-      <UserInput />
-    </div>
-  );
+class App extends Component {
+  state = {
+    persons: [
+      {name: 'Bella', age: 17},
+      {name: 'William', age: 22}
+    ]
+  }
+
+  render() {
+    return (
+      <div className="App">
+        Hello
+        <UserInput />
+        <UserOutput name={this.state.persons[0].name} age={this.state.persons[0].age} />
+        <UserOutput name={this.state.persons[1].name} age={this.state.persons[1].age}/>
+      </div>
+    );
+  }
 }
-
 export default App;
--- src/UserOutput.js
+++ src/UserOutput.js
@@ -1,1 +1,12 @@
 import React from 'react';
+
+function UserOutput(props) {
+
+    return (
+    <div>
+    <p> {props.name} {props.age}</p>
+    </div>
+    ) 
+  };
+  
+  export default UserOutput;
